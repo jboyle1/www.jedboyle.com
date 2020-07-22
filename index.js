673c0e3a234e499060240b3fb212c4ef572994c3
Jed Boyle <jedboyle01@gmail.com>
2020-07-22 18:30:19 +0100

    Create 'public' folder and add 'index.html'. In 'index.html' add boilerplate code. Run to test in browser.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-// Require express 
+// Require 'express'
 const express = require('express');
 // Place express module in a variable called 'app'.
 const app = express();
 
+// Require 'path'
+const path = require('path');
+
 // Create route to index.html
 app.get('/', (req, res) => {
-    res.send('test')
+    res.sendfile(path.join(__dirname, 'public', 'index.html'))
 });
 
 // To run the web server, create a variable called PORT that evaluates to a hosted port or a local port
