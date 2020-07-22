0b094b7286c5ed010869d55ed833bbb5179bd4f5
Jed Boyle <jedboyle01@gmail.com>
2020-07-22 18:21:01 +0100

    Create first route and test it using the res.send('test').

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const express = require('express');
 // Place express module in a variable called 'app'.
 const app = express();
 
+// Create route to index.html
+app.get('/', (req, res) => {
+    res.send('test')
+});
+
 // To run the web server, create a variable called PORT that evaluates to a hosted port or a local port
 const PORT = process.env.PORT || 5000;
 // run express 'listen' on 'app' on 'PORT' with a callback function second parameter that console logs that the server is running on the specific port.
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
 
