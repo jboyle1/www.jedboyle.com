d229590a3bef3807962868768f3b8764b13fc9f8
Jed Boyle <jedboyle01@gmail.com>
2020-12-12 14:38:18 +0000

    Fix bug.

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 // Require 'express' for templating.
-// Require handlebars for data templating.
-const exphbs = require('express-handlebars');
-// Require 'fs' for PDF viewer.
-const fs = require('fs')
-
-const https = require('https');
-
 const express = require('express');
 // Place express module in a variable called 'app'.
-
-const NODE_ENV = process.env.NODE_ENV || 'development';
-const PORT = process.env.PORT || 5000
 const app = express();
 // Require path for static file use.
 const path = require('path');
-
-
-
+// Require handlebars for data templating.
+const exphbs = require('express-handlebars');
+// Require 'fs' for PDF viewer.
+fs = require('fs')
 
 // Create public middleware so static files are accessible.
 app.use(express.static(path.join(__dirname, 'public/css')));
@@ -1019,21 +1010,9 @@ app.get('/dry-ux', function (req, res) {
     });
 });
 
-// // To run the web server, create a variable called PORT that evaluates to a
-// // hosted port or a local port
-// const PORT = process.env.PORT || 5000;
-// // run express 'listen' on 'app' on 'PORT' with a callback function second
-// // parameter that console logs that the server is running on the specific port.
-// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-app.listen(PORT);
-// Redirect http requests to use https in production
-if (NODE_ENV === 'production') {
-  app.use((req, res, next) => {
-    if (req.header('x-forwarded-proto') !== 'https') {
-      res.redirect(`https://${req.header('host')}${req.url}`);
-    } else {
-      next();
-    }
-  });
-}
+// To run the web server, create a variable called PORT that evaluates to a
+// hosted port or a local port
+const PORT = process.env.PORT || 5000;
+// run express 'listen' on 'app' on 'PORT' with a callback function second
+// parameter that console logs that the server is running on the specific port.
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
